3214d52eecee49c5dc3b8439539c1d15a51593c6
huanghui <120925502@qq.com>
2023-08-18 17:46:41 +0800

    going...

--- lib/ScaffoldDemo/scaffold_demo.dart
+++ lib/ScaffoldDemo/scaffold_demo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ScaffoldDemo extends StatefulWidget {
   const ScaffoldDemo({super.key});
@@ -8,12 +9,13 @@ class ScaffoldDemo extends StatefulWidget {
 }
 
 class _ScaffoldDemoState extends State<ScaffoldDemo> with TickerProviderStateMixin {
-  late int _currentTabIndex = 0;
+  final int _currentTabIndex = 0;
   late bool _isAnimation = false;
   late TabController _tabController;
   late Animation<Color?> _animationColor;
   late Animation<double> _animationIcon;
   late AnimationController _animationController;
+  late TextEditingController _phoneController;
 
   final Curve _curve = Curves.easeOut;
   final GlobalKey<ScaffoldState> _scaffoldStateKey = GlobalKey();
@@ -24,31 +26,39 @@ class _ScaffoldDemoState extends State<ScaffoldDemo> with TickerProviderStateMix
 
   @override
   void initState() {
-    super.initState();
-
     // 初始化tabBar的控制器
     _tabController = TabController(length: 2, vsync: this);
 
     // 初始化动画控制器
-    _animationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 500))
-      ..addListener(() {
-        setState(() {});
-      });
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(milliseconds: 500))
+          ..addListener(() {
+            setState(() {});
+          });
 
     // 初始化颜色
     _animationColor = ColorTween(
       end: const Color.fromARGB(255, 177, 88, 236),
       begin: const Color.fromARGB(99, 222, 15, 237),
-    ).animate(CurvedAnimation(parent: _animationController, curve: Interval(0.0, 1.0, curve: _curve)));
+    ).animate(
+      CurvedAnimation(parent: _animationController, curve: Interval(0.0, 1.0, curve: _curve)),
+    );
 
     // 初始化icon
     _animationIcon = Tween(begin: 0.0, end: 1.0).animate(_animationController);
+
+    // 初始化手机号码输入的控制器
+    _phoneController = TextEditingController();
+
+    super.initState();
   }
 
   @override
   void dispose() {
     super.dispose();
+    _tabController.dispose();
     _animationController.dispose();
+    _phoneController.dispose();
   }
 
   @override
@@ -58,7 +68,7 @@ class _ScaffoldDemoState extends State<ScaffoldDemo> with TickerProviderStateMix
       appBar: AppBar(
         title: const Text('Scaffold'),
         backgroundColor: const Color.fromARGB(255, 220, 201, 240),
-        titleTextStyle: const TextStyle(color: Colors.white, fontSize: 20),
+        titleTextStyle: const TextStyle(color: Colors.white, fontSize: 30),
         iconTheme: const IconThemeData(color: Colors.white),
         leading: TextButton(
           onPressed: () {
@@ -80,7 +90,13 @@ class _ScaffoldDemoState extends State<ScaffoldDemo> with TickerProviderStateMix
           isScrollable: true,
         ),
       ),
-      body: _pages[_currentTabIndex],
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () {
+          debugPrint('空白区域被点击了');
+        },
+        child: _pages[_currentTabIndex],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           _isAnimation ? _animationController.reverse() : _animationController.forward();
@@ -126,14 +142,27 @@ class _ScaffoldDemoState extends State<ScaffoldDemo> with TickerProviderStateMix
         width: MediaQuery.of(context).size.width - 0,
         child: Row(
           children: [
-            const Expanded(
+            Expanded(
               child: TextField(
-                decoration: InputDecoration(
+                controller: _phoneController,
+                keyboardType: TextInputType.number,
+                cursorColor: Colors.green,
+                cursorWidth: 2,
+                inputFormatters: [
+                  LengthLimitingTextInputFormatter(11),
+                  FilteringTextInputFormatter.allow(RegExp('[0-9]'))
+                ],
+                decoration: const InputDecoration(
+                  hintText: '请输入手机号码',
+                  border: InputBorder.none,
                   contentPadding: EdgeInsets.symmetric(horizontal: 20),
                 ),
               ),
             ),
-            TextButton(onPressed: () {}, child: const Text('发送')),
+            TextButton(
+              onPressed: () {},
+              child: const Text('发送'),
+            ),
           ],
         ),
       ),
